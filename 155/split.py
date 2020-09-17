a626c5368c1ba4b1b935f61933c9c70e2b045007
piotrrybinski88 <piotrrybinski88@gmail.com>
2020-09-17 12:29:35 +0200

    bites 155

---
+++ 155/split.py
@@ -0,0 +1,22 @@
+import re
+
+
+def split_words_and_quoted_text(text):
+    """Split string text by space unless it is
+       wrapped inside double quotes, returning a list
+       of the elements.
+
+       For example
+       if text =
+       'Should give "3 elements only"'
+
+       the resulting list would be:
+       ['Should', 'give', '3 elements only']
+    """
+    text_with_double_quates = re.findall(r'\".+\"', text)[0]
+    text_without_double_quates = text.replace(text_with_double_quates, '').split(' ')
+    for num, word in enumerate(text_without_double_quates):
+        if word == '':
+            text_without_double_quates[num] = text_with_double_quates.strip('"')
+
+    return text_without_double_quates
